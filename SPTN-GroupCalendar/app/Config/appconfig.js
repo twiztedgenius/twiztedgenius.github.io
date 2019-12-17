0406e607594eabbd02b5c57d93896fc530f1c41a
twiztedgenius <58953651+twiztedgenius@users.noreply.github.com>
2019-12-17 13:28:02 -0600

    Add files via upload

---
+++ SPTN-GroupCalendar/app/Config/appconfig.js
@@ -0,0 +1,9 @@
+const getConfig = () => {
+   var config = {
+        clientId : "296a4cd3-c2fd-489b-a7a9-c3f2ab930008",
+        redirectUri : "/SPTN-GroupCalendar/app/silent-end.html",
+        authwindow :  "/SPTN-GroupCalendar/app/auth.html",
+	hostRoot: "https://twiztedgenius.github.io",
+   };
+   return config;
+}

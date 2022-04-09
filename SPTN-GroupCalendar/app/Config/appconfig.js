94eefe9b951b7c9c3828633137d8e518ba09dd63
twiztedgenius <58953651+twiztedgenius@users.noreply.github.com>
2022-04-09 15:45:57 -0500

    Delete SPTN-GroupCalendar directory

--- SPTN-GroupCalendar/app/Config/appconfig.js
+++
@@ -1,9 +0,0 @@
-const getConfig = () => {
-   var config = {
-        clientId : "296a4cd3-c2fd-489b-a7a9-c3f2ab930008",
-        redirectUri : "/SPTN-GroupCalendar/app/silent-end.html",
-        authwindow :  "/SPTN-GroupCalendar/app/auth.html",
-	hostRoot: "https://twiztedgenius.github.io",
-   };
-   return config;
-}
